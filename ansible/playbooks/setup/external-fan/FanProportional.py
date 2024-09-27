dbfb782cf5e4573eae2bc00d74e7c9edae57346b
Krzysztof Czarnecki <czarneckikrzysztof97@gmail.com>
2024-09-27 12:39:43 +0200

    feat: dodanie skryptu do zewnętrznego wentylatorka w raspberyy

---
+++ ansible/playbooks/setup/external-fan/FanProportional.py
@@ -0,0 +1,34 @@
+import subprocess  # Calling subprocess to get the CPU temperature
+from gpiozero import PWMLED
+from time import sleep
+
+fan = PWMLED(14)
+
+
+# Function to read in the CPU temperature and return it as a float in degrees celcius
+def get_temp():
+  output = subprocess.run(['vcgencmd', 'measure_temp'], capture_output=True)
+  temp_str = output.stdout.decode()
+  try:
+    return float(temp_str.split('=')[1].split('\'')[0])
+  except (IndexError, ValueError):
+    raise RuntimeError('Could not get temperature')
+
+
+def calculate_fan_speed(temp, min_temp=30, max_temp=70):
+  if temp < min_temp:
+    return 0
+  elif temp > max_temp:
+    return 100
+  else:
+    # Oblicz proporcjonalną wartość w zakresie 0-100
+    return (temp - min_temp) / (max_temp - min_temp) * 100
+
+
+
+while 1:
+  temp = get_temp()
+  fan_speed = calculate_fan_speed(temp)
+  fan.value = fan_speed / 100
+  print(f"Temp: {temp}. Fan speed: {fan_speed}")
+  sleep(1)
